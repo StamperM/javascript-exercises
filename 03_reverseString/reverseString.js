f256c7e4e9cc1bb256ac0e6d7fdf5df146faaa9f
StamperM <90412072+StamperM@users.noreply.github.com>
2022-11-15 21:06:16 -0500

    changes to remove array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(str) {
+const spiltStr = str.split('');
+const reverseStr = spiltStr.reverse();
+const joinArray = reverseStr.join('');
+return joinArray;   
+}
 // Do not edit below this line
 module.exports = reverseString;
+
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
+  let sum = '';
+    for(i= int1; i <= int2; i++) {
+    sum += sum[i];
+} 
+return sum + int1;
 
 };
+//int1+ sum 
 
 // Do not edit below this line
 module.exports = sumAll;
